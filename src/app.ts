b595b2478ad27c93a08b49e3371584d73ced3df9
Ivan <Priest.ivan14@gmail.com>
2022-10-30 15:17:40 +0300

    Refactoring JS in TS Project #2. Проведена типизация функций, ошибок и т.д.

--- src/app.ts
+++ src/app.ts
@@ -22,17 +22,18 @@ interface User {
 
   // Attach Events
   document.addEventListener('DOMContentLoaded', initApp);
-  form.addEventListener('submit', handleSubmit);
+  form?.addEventListener('submit', handleSubmit);
 
   // Basic Logic
   function getUserName(userId: ID) {
     const user = users.find((u) => u.id === userId);
-    return user.name;
+    return user?.name || "";
   }
+
   function printTodo({id, userId, title, completed}:Todo) {
     const li = document.createElement('li');
     li.className = 'todo-item';
-    li.dataset.id = id;
+    li.dataset.id = String(id);
     li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(
       userId
     )}</b></span>`;
@@ -50,26 +51,32 @@ interface User {
     li.prepend(status);
     li.append(close);
 
-    todoList.prepend(li);
+    todoList?.prepend(li);
   }
 
   function createUserOption(user: User) {
+    if (userSelect){
     const option = document.createElement('option');
-    option.value = user.id;
+    option.value = String(user.id);
     option.innerText = user.name;
 
     userSelect.append(option);
+    }
   }
 
   function removeTodo(todoId: ID) {
+    if(todoList){
     todos = todos.filter((todo) => todo.id !== todoId);
-
     const todo = todoList.querySelector(`[data-id="${todoId}"]`);
-    todo.querySelector('input').removeEventListener('change', handleTodoChange);
-    todo.querySelector('.close').removeEventListener('click', handleClose);
+
+    if(todo){
+    todo.querySelector('input')?.removeEventListener('change', handleTodoChange);
+    todo.querySelector('.close')?.removeEventListener('click', handleClose);
 
     todo.remove();
-  }
+   }
+ }
+}
 
   function alertError(error: Error) {
     alert(error.message);
@@ -85,24 +92,34 @@ interface User {
       users.forEach((user) => createUserOption(user));
     });
   }
-  function handleSubmit(event) {
+  function handleSubmit(event: Event) {
     event.preventDefault();
 
+    if(form){
     createTodo({
       userId: Number(form.user.value),
       title: form.todo.value,
       completed: false,
     });
+   }
   }
-  function handleTodoChange() {
-    const todoId = this.parentElement.dataset.id;
+
+  function handleTodoChange(this: HTMLInputElement) {
+    const parent = this.parentElement
+    if(parent){
+    const todoId = parent.dataset.id;
     const completed = this.checked;
 
-    toggleTodoComplete(todoId, completed);
+    todoId && toggleTodoComplete(todoId, completed);
+    }
   }
-  function handleClose() {
-    const todoId = this.parentElement.dataset.id;
-    deleteTodo(todoId);
+
+  function handleClose(this: HTMLSpanElement) {
+    const parent = this.parentElement
+    if(parent){
+    const todoId =  parent.dataset.id;
+    todoId && deleteTodo(todoId);
+    }
   }
 
   // Async logic
@@ -115,6 +132,7 @@ interface User {
 
       return data;
     } catch (error) {
+      if(error instanceof Error)
       alertError(error);
     }
   }
@@ -128,11 +146,12 @@ interface User {
 
       return data;
     } catch (error) {
+      if(error instanceof Error)
       alertError(error);
     }
   }
 
-  async function createTodo(todo: Todo) {
+  async function createTodo(todo: Omit <Todo, 'id'>) {
     try {
       const response = await fetch(
         'https://jsonplaceholder.typicode.com/todos',
@@ -149,11 +168,12 @@ interface User {
 
       printTodo(newTodo);
     } catch (error) {
+      if(error instanceof Error)
       alertError(error);
     }
   }
 
-  async function toggleTodoComplete(todoId: ID, completed) {
+  async function toggleTodoComplete(todoId: ID, completed: boolean) {
     try {
       const response = await fetch(
         `https://jsonplaceholder.typicode.com/todos/${todoId}`,
@@ -170,6 +190,7 @@ interface User {
         throw new Error('Failed to connect with the server! Please try later.');
       }
     } catch (error) {
+      if(error instanceof Error)
       alertError(error);
     }
   }
@@ -192,6 +213,7 @@ interface User {
         throw new Error('Failed to connect with the server! Please try later.');
       }
     } catch (error) {
+      if(error instanceof Error)
       alertError(error);
     }
   }
